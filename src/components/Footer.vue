4335bc50d0a038b5acc2966496e2ee2d13d352c9
Pavels Ivanovs <paulivanov586@gmail.com>
2022-06-03 13:47:24 +0300

    Footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="footer">
+        <p>Pavels Ivanovs, pi19003</p>
+        <p>Rīga, Latvija | 2022</p>
+    </div>
+</template>
+
+<style>
+.footer {
+    background-color: #65eaea;
+    padding: 20px;
+}
+</style>
