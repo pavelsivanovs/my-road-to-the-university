4580e4226beefb897babb8b97aeac047d0256ed4
Pavels Ivanovs <paulivanov586@gmail.com>
2022-06-03 13:50:24 +0300

    LanguageSwicther component implementation

---
+++ src/components/LanguageSwitcher.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+
+let url = document.location.pathname
+let title = "lv"
+
+if (url.substring(0, 3) != "/en") {
+    url = "/en" + url
+    title = "en"
+}
+
+console.log(url)
+</script>
+
+<template>
+<div class="language-switcher">
+    <RouterLink class="language" :to=url>
+        {{ title }}
+    </RouterLink>
+</div>
+</template>
+
+<style scoped>
+.language {
+    color: #000;
+    font-size: 24px;
+    font-weight: 800;
+    position: absolute;
+    text-decoration: none;
+    top: 20px;
+    right: 20px;
+}
+
+.language:hover {
+    text-decoration: underline;
+}
+</style>
