98fcdaf9c5c8057a069c78c1e35dc405c2a691c4
Pavels Ivanovs <paulivanov586@gmail.com>
2022-06-03 14:02:16 +0300

    Basic styles

--- src/App.vue
+++ src/App.vue
@@ -10,13 +10,35 @@ import HelloWorld from './components/HelloWorld.vue'
 </template>
 
 <style>
+@font-face {
+  font-family: e-Ukraine;
+  src: url(./assets/fonts/e-Ukraine-Light.woff2);
+}
+
+@font-face {
+  font-family: KyivPost;
+  src: url(./assets/fonts/KyivTypeSerif-Bold3.woff2);
+}
+
+:root {
+  --yellow: #edde80;
+  --cyan: #65eaea;
+  --magenta: #f384bc;
+
+  --desktop-screen: 1040px;
+}
+
+html, body {
+  background-color: var(--cyan);
+  min-height: 100%; 
+  margin: 0;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: e-Ukraine, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  background-color: #edde80;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #000;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,3 +3,55 @@
         <h1>Labdien!</h1>
     </div>
 </template>
+
+<style scoped>
+.header {
+  background-color: var(--yellow);
+  padding: 50px 0;
+}
+
+.content {
+  display: flex;
+  flex-direction: row;
+  gap: 60px;
+  margin: 0 auto;
+  max-width: var(--desktop-screen);
+}
+
+.content-item {
+  flex-grow: 1;
+}
+
+.logo {
+  max-width: 300px;
+}
+
+.title-wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.title-wrapper > h1 {
+  font-size: 80px;
+  font-weight: 400;
+  height: fit-content;
+  max-width: 600px;
+  text-align: left;
+  width: fit-content;
+}
+
+.sections {
+  display: flex;
+  margin: 0 auto;
+  max-width: var(--desktop-screen);
+  flex-wrap: wrap;
+  flex-direction: row;
+  gap: 20px;
+}
+
+.main {
+  background-color: var(--magenta);
+  padding: 100px 0;
+}
+</style>
