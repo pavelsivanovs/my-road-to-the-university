dfa750cb39aa82b8814089865234d2be22bf68b2
Pavels Ivanovs <paulivanov586@gmail.com>
2022-06-03 13:50:54 +0300

    Attempt to add CSS nesting

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,21 @@
 import { defineConfig } from 'vite'
+import postcssNesting from 'postcss-nesting'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [
+        postcssNesting
+      ]
+    }
+  },
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
+      '@': path.resolve(__dirname, './src'),
     }
   }
 })
