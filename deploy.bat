14ecade09ff7ec572baeb94a6b393a9b6f16ab96
Pavels Ivanovs <paulivanov586@gmail.com>
2022-06-04 23:31:01 +0300

    Deployment script

---
+++ deploy.bat
@@ -0,0 +1,14 @@
+:: Batch files prepares files in the \dist directory and deploys them to GitHub.
+
+ECHO Creating a 404.html file
+COPY dist\index.html dist\404.html
+
+ECHO Adding files in the dist directory and making a commit
+CALL git add -f dist && git commit -m "deploy"
+
+ECHO Pushing the build to the origin/gh-pages subtree
+CALL git subtree push --prefix dist origin gh-pages
+
+ECHO ====================
+ECHO Deployment commenced
+ECHO ====================
